8c98549785cb0fc862d04c7b0d92a074f40ef6b4
akin tanis <wakeupneo9@gmail.com>
2022-08-08 01:02:19 +0200

    "GET /" and "POST /weather" endpoints added

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+  res.send("hello from backend to frontend!");
+});
+
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.setHeader("Content-Type", "text/plain");
+  res.send(cityName);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
